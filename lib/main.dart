25a0c89f9bc02076f60e37d8a0f6ac6df31462e0
Raghad Narech <raghad.j.narech@gmail.com>
2024-09-02 13:54:48 +0300

    edit bundleId and fix some bug

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,9 @@ import 'package:baba_karkar/controller/ScheduleProvider.dart';
 import 'package:baba_karkar/controller/ServicesProvider.dart';
 import 'package:baba_karkar/controller/StrudentProvider.dart';
 import 'package:baba_karkar/controller/TeacherProvider.dart';
-
 import 'package:baba_karkar/l10n/l10n.dart';
 import 'package:baba_karkar/view/Splash/splash.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -22,7 +20,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   SharedPreferences preferences = await SharedPreferences.getInstance();
   bool isLogged = preferences.getBool("isLogged") ?? false;
   String? localeCode = preferences.getString('lang');
